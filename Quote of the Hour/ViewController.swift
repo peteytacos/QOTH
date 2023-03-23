380106a9ad119b021c6b8c050c1d29f29e999172
Pete <p.wallroth@yahoo.com>
2023-03-23 14:20:16 +1100

    Initial Commit

---
+++ Quote of the Hour/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Quote of the Hour
+//
+//  Created by Peter Wallroth on 23/3/2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ Quote of the HourUITests/Quote_of_the_HourUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Quote_of_the_HourUITestsLaunchTests.swift
+//  Quote of the HourUITests
+//
+//  Created by Peter Wallroth on 23/3/2023.
+//
+
+import XCTest
+
+final class Quote_of_the_HourUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
